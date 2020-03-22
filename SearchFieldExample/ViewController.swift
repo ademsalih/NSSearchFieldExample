1f6c176f6b32a2295ebdc72791f134735eecda5d
Adem Salih <ademsal.97@icloud.com>
2020-03-22 20:59:31 +0100

    initial commit

---
+++ SearchFieldExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SearchFieldExample
+//
+//  Created by Adem Salih on 22/03/2020.
+//  Copyright © 2020 Adem Salih. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
